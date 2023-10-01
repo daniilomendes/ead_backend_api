36c9dcc0c8c15ca66f3691dcf6bb6311cce2dad0
Danilo Mendes <danilo.mirandha@gmail.com>
2023-10-01 16:34:09 -0300

    Obtendo cursos de uma categoria

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -3,6 +3,7 @@ import { categoryService } from "../services/categoryService";
 import { getPaginationParams } from "../helpers/getPaginationParams";
 
 export const categoriesController = {
+    // GET /categories
     index: async (req: Request, res: Response) => {
         const [page, perPage] = getPaginationParams(req.query)
 
@@ -10,6 +11,20 @@ export const categoriesController = {
             const paginatedCategories = await categoryService.findAllPaginated(page, perPage)
 
             return res.json(paginatedCategories)
+        } catch (error) {
+            if (error instanceof Error) {
+                return res.status(400).json({ message: error.message })
+            }
+        }
+    },
+
+    //GET /categories/:id
+    show: async (req: Request, res: Response) => {
+        const {id} = req.params
+
+        try {
+            const category = await categoryService.findByIdWithCourses(id)
+            return res.json(category)
         } catch (error) {
             if (error instanceof Error) {
                 return res.status(400).json({ message: error.message })
--- src/models/index.ts
+++ src/models/index.ts
@@ -4,9 +4,11 @@ import { Episode } from "./Episode";
 import { User } from './User'
 
 
-Category.hasMany(Course)
+Category.hasMany(Course, { as: 'courses' })
+
 Course.belongsTo(Category)
 Course.hasMany(Episode)
+
 Episode.belongsTo(Course)
 
 
