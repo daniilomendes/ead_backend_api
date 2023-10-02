f4de29efc0f96997ff260ff9c36619bd1a9210b8
Danilo Mendes <danilo.mirandha@gmail.com>
2023-10-02 13:32:14 -0300

    Obtendo os 10 cursos mais populares

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -30,6 +30,18 @@ export const coursesController = {
         }
     },
 
+    // GET /courses/popular
+    popular: async (req: Request, res: Response) => {
+        try {
+            const topTen = await courseService.getTopTenByLikes()
+            return res.json(topTen)
+        } catch (error) {
+            if (error instanceof Error) {
+                return res.status(400).json({ message: error.message })
+            }
+        }
+    },
+
     // GET /courses/search?name=
     search: async (req: Request, res: Response) => {
         const { name } = req.query
