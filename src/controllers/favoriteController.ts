80d6706eb1130fece0267a9aab10303158b5844f
Danilo Mendes <danilo.mirandha@gmail.com>
2023-10-02 10:45:44 -0300

    Excluindo um favorito

--- src/controllers/favoriteController.ts
+++ src/controllers/favoriteController.ts
@@ -30,5 +30,20 @@ export const favoritesController = {
                 return res.status(400).json({ message: error.message })
             }
         }
+    },
+
+    // DELETE /favorites/:id
+    delete: async (req: AuthenticatedRequest, res: Response) => {
+        const userId = req.user!.id
+        const courseId = req.params.id
+
+        try {
+            await favoriteService.delete(userId, Number(courseId))
+            return res.status(204).send()
+        } catch (error) {
+            if (error instanceof Error) {
+                return res.status(400).json({ message: error.message })
+            }
+        }
     }
 }
